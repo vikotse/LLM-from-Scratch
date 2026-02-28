e46d1e8d5d72e3773da4ba42bb4f1f0dbbd5db66
vikotse <tsewkviko@gmail.com>
2026-02-28 12:03:23 +0800

    update learning_rate_tuning

---
+++ assignment1-basics/script/learning_rate_tuning.py
@@ -0,0 +1,21 @@
+import torch
+
+from src.optimizer import SGD
+
+def run_lr_tuning(lr=1):
+    weights = torch.nn.Parameter(5 * torch.randn((10, 10)))
+    opt = SGD([weights], lr=lr)
+    # lr = 1e1, 1e2 decay getting faster; lr = 1e3 getting diverge 
+    print(f"lr: {lr}")
+    for t in range(100):
+        opt.zero_grad() # Reset the gradients for all learnable parameters.
+        loss = (weights**2).mean() # Compute a scalar loss value.
+        print(loss.cpu().item())
+        loss.backward() # Run backward pass, which computes gradients.
+        opt.step() # Run optimize
+    print("---------")
+
+if __name__ == '__main__':
+    lrs = [1, 1e1, 1e2, 1e3]
+    for lr in lrs:
+        run_lr_tuning(lr=lr)
--- assignment1-basics/src/optimizer.py
+++ assignment1-basics/src/optimizer.py
@@ -102,15 +102,3 @@ def gradient_clipping(
             if param.grad is not None:
                 param.grad.mul_(clip_factor)
     return
-
-
-if __name__ == '__main__':
-    weights = torch.nn.Parameter(5 * torch.randn((10, 10)))
-    opt = SGD([weights], lr=1)
-    # lr = 1e1, 1e2 decay getting faster; lr = 1e3 getting diverge 
-    for t in range(100):
-        opt.zero_grad() # Reset the gradients for all learnable parameters.
-        loss = (weights**2).mean() # Compute a scalar loss value.
-        print(loss.cpu().item())
-        loss.backward() # Run backward pass, which computes gradients.
-        opt.step() # Run optimizer step.
